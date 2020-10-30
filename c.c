6ffe1dfbaf03df8ff5ed61dee0dbf23a197b1f3b
Ingo Albrecht <commit@promovicz.org>
2020-10-30 15:43:18 +0100

    Add copyright line

--- c.c
+++ c.c
@@ -1,6 +1,8 @@
 /*
  * c - utility for running C code
  *
+ * Copyright (C) 2020 Ingo Albrecht <copyright@promovicz.org>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
