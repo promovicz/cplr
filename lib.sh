390c87019f215a91f89d3aed6e5c50cd9336c4ef
Ingo Albrecht <commit@promovicz.org>
2020-11-01 07:38:06 +0100

    Untabify

--- lib.sh
+++ lib.sh
@@ -21,38 +21,38 @@ say() {
 
 quote() {
     for s in "$@"; do
-	printf "${sep}%q" "${s}"
-	sep=" "
+        printf "${sep}%q" "${s}"
+        sep=" "
     done
 }
 
 quoteline() {
     local sep=""
     while read -r line; do
-	echo -n "${sep}"
-	quote "${line}"
+        echo -n "${sep}"
+        quote "${line}"
     done
 }
 
 nquote() {
     local sep=""
     for s in "$@"; do
-	local e="$(quote "${s}")"
-	if [ "${s}" != "${e}" ]; then
-	    echo -n "${sep}'${s}'"
-	else
-	    echo -n "${sep}${s}"
-	fi
-	sep=" "
+        local e="$(quote "${s}")"
+        if [ "${s}" != "${e}" ]; then
+            echo -n "${sep}'${s}'"
+        else
+            echo -n "${sep}${s}"
+        fi
+        sep=" "
     done
 }
 
 nquoteline() {
     local sep=" "
     while read -r line; do
-	echo -n "${sep}"
-	nquote "${line}"
-	sep=" "
+        echo -n "${sep}"
+        nquote "${line}"
+        sep=" "
     done
 }
 
@@ -60,10 +60,10 @@ run_editor() {
     local file="$1"
     local we=""
     for e in sensible-editor editor ${EDITOR} vim vi nano pico; do
-	we="$(which "${e}")"
-	if [ -n "${we}" ]; then
-	    "${we}" "${file}"
-	fi
+        we="$(which "${e}")"
+        if [ -n "${we}" ]; then
+            "${we}" "${file}"
+        fi
         return 0
     done
     echo "Could not find an editor"
