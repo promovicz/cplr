54d0d72497bb2d75b18e797f69f28de6bd749b25
Ingo Albrecht <commit@promovicz.org>
2020-11-01 03:26:36 +0100

    More default headers
    Going to have to do something about spamming -d output.

--- c.c
+++ c.c
@@ -541,9 +541,12 @@ int cplr_defaults(cplr_t *c) {
   l_appends(&c->defdef, "-D_POSIX_SOURCE=200809L");
   l_appends(&c->defsys, "sys/stat.h");
   l_appends(&c->defsys, "sys/types.h");
+  l_appends(&c->defsys, "assert.h");
   l_appends(&c->defsys, "ctype.h");
   l_appends(&c->defsys, "errno.h");
   l_appends(&c->defsys, "fcntl.h");
+  l_appends(&c->defsys, "libgen.h");
+  l_appends(&c->defsys, "limits.h");
   l_appends(&c->defsys, "stdarg.h");
   l_appends(&c->defsys, "stdio.h");
   l_appends(&c->defsys, "stdint.h");
