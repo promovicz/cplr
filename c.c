86174c92221bf337c5e11b279fff034950f069aa
Ingo Albrecht <commit@promovicz.org>
2020-11-01 03:25:18 +0100

    Now it's vsmnprintf and even better

--- c.c
+++ c.c
@@ -33,6 +33,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <getopt.h>
+#include <limits.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -66,22 +67,38 @@ void xfree(void **p) {
   *p = NULL;
 }
 
-char *smnprintf(size_t limit, const char *fmt, ...) {
+char *vsmnprintf(size_t limit, const char *fmt, va_list a) {
   int len, chk;
   char *buf;
-  va_list a;
-  va_start(a, fmt);
-  len = vsnprintf(NULL, 0, fmt, a);
-  va_end(a);
-  if(len > limit) {
+  va_list x;
+  if(limit > INT_MAX) {
+    limit = INT_MAX;
+  }
+  va_copy(x, a);
+  len = vsnprintf(NULL, 0, fmt, x);
+  va_end(x);
+  if(len > (int)limit) {
     return NULL;
   }
-  buf = xmalloc(1, len);
-  va_start(a, fmt);
-  chk = vsnprintf(buf, res, fmt, a);
-  va_end(a);
+  buf = malloc(len + 1);
+  if(!buf) {
+    return NULL;
+  }
+  va_copy(x, a);
+  chk = vsnprintf(buf, len + 1, fmt, x);
+  va_end(x);
   assert(chk == len);
+  fprintf(stderr, "fmt \"%s\" res \"%s\"\n", fmt, buf);
   return buf;
+}
+
+char *smnprintf(size_t limit, const char *fmt, ...) {
+  char *res;
+  va_list a;
+  va_start(a, fmt);
+  res = vsmnprintf(limit, fmt, a);
+  va_end(a);
+  return res;
 }
 
 typedef struct lh lh_t;
