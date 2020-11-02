2dd31043ca66c264c812c2cb504c7c55d4008d57
Ingo Albrecht <commit@promovicz.org>
2020-11-02 08:12:46 +0100

    Free memory for now

--- source/cplr_execute.c
+++ source/cplr_execute.c
@@ -2,12 +2,14 @@
 #include "cplr.h"
 
 int cplr_execute(cplr_t *c) {
-  int i;
+  int i, ret;
   int argc = 1 + (c->argc - c->argp);
   char **argv = xcalloc(sizeof(char*), argc+1);
   argv[0] = "c";
   for(i = 1; i < argc; i++) {
     argv[i] = c->argv[c->argp + (i - 1)];
   }
-  return tcc_run(c->tcc, argc, argv);
+  ret = tcc_run(c->tcc, argc, argv);
+  xfree(argv);
+  return ret;
 }
