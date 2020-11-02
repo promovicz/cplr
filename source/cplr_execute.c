7dd5564eb0a07d61bec749354928fae7da2404c6
Ingo Albrecht <commit@promovicz.org>
2020-11-02 10:37:55 +0100

    Comments

--- source/cplr_execute.c
+++ source/cplr_execute.c
@@ -3,13 +3,16 @@
 
 int cplr_execute(cplr_t *c) {
   int i, ret;
+  /* copy arguments */
   int argc = 1 + (c->argc - c->argp);
   char **argv = xcalloc(sizeof(char*), argc+1);
   argv[0] = "c";
   for(i = 1; i < argc; i++) {
     argv[i] = c->argv[c->argp + (i - 1)];
   }
+  /* run the program */
   ret = tcc_run(c->tcc, argc, argv);
+  /* free arguments */
   xfree(argv);
   return ret;
 }
