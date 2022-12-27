e659a7ec1d571e088dd437e3776899453410e4c6
Ingo Albrecht <commit@promovicz.org>
2022-12-27 05:16:11 +0100

    More attributes

--- include/cext/attr.h
+++ include/cext/attr.h
@@ -20,29 +20,66 @@
 #ifndef CPLR_ATTR_H
 #define CPLR_ATTR_H
 
+#define ATTR_FUN_DEPRECATED                     \
+  __attribute__((deprecated))
+#define ATTR_FUN_UNAVAILABLE			\
+  __attribute__((unavailable))
+
 #define ATTR_FUN_ARTIFICIAL			\
   __attribute__((artificial))
 #define ATTR_FUN_COLD				\
   __attribute__((cold))
 #define ATTR_FUN_CONST				\
   __attribute__((const))
+#define ATTR_FUN_FLATTEN                        \
+  __attribute__((flatten))
+#define ATTR_FUN_LEAF				\
+  __attribute__((leaf))
+#define ATTR_FUN_NOIPA				\
+  __attribute__((noipa))
 #define ATTR_FUN_PURE				\
   __attribute__((pure))
-#define ATTR_FUN_MALLOC				\
-  __attribute__((malloc))
+
 #define ATTR_FUN_NORETURN			\
   __attribute__((noreturn))
+#define ATTR_FUN_NOTHROW			\
+  __attribute__((nothrow))
 #define ATTR_FUN_RETURNS_NONNULL		\
   __attribute__((returns_nonnull))
 #define ATTR_FUN_RETURNS_TWICE			\
   __attribute__((returns_twice))
-#define ATTR_FUN_FORMAT(arch, fmtindex, argindex)       \
-  __attribute__((format(arch, fmtindex, argindex)))
+
+#define ATTR_FUN_UNUSED				\
+  __attribute__((unused))
+#define ATTR_FUN_USED				\
+  __attribute__((used))
+
+
+#define ATTR_FUN_MALLOC				\
+  __attribute__((malloc))
 #define ATTR_ARG_ALLOC_SIZE(...)		\
   __attribute__((alloc_size(__VA_ARGS__)))
+
+#define ATTR_FUN_FORMAT(arch, fmtindex, argindex)       \
+  __attribute__((format(arch, fmtindex, argindex)))
 #define ATTR_ARG_FORMAT(...)			\
   __attribute__((format_arg(__VA_ARGS__)))
+
 #define ATTR_ARG_NONNULL(...)			\
   __attribute__((nonnull(__VA_ARGS__)))
+
+#define ATTR_ARG_READONLY(...)                          \
+  __attribute__((access(read_only,__VA_ARGS__)))
+#define ATTR_ARG_WRITEONLY(...)                         \
+  __attribute__((access(write_only,__VA_ARGS__)))
+
+#define ATTR_ARG_SENTINEL(...)			\
+  __attribute__((sentinel(__VA_ARGS__)))
+
+#define ATTR_RET_ASSUME_ALIGNED(...)            \
+  __attribute__((assume_aligned(__VA_ARGS__)))
+
+#define ATTR_RET_WARN_UNUSED(...)            \
+  __attribute__((warn_unused_result(__VA_ARGS__)))
 
 #endif /* !CPLR_ATTR_H */
